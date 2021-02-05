ec6d8a8ece844db8f4362b9f190f71b23c777644
Hasib787 <mr.hasib45@gmail.com>
2021-02-06 02:00:04 +0600

    added some method bind call apply

--- call-apply.js
+++ call-apply.js
@@ -5,14 +5,14 @@ const normalPerson ={
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
-        this.salary= this.salary - amount;
+    chargeBill: function(amount, tips, tax){
+        this.salary= this.salary - amount - tips - tax;
         return this.salary;
     }
 }
-// normalPerson.chargeBill(100);
-// normalPerson.chargeBill(2000);
-// console.log(normalPerson.salary);
+normalPerson.chargeBill(100);
+normalPerson.chargeBill(2000);
+console.log(normalPerson.salary);
 
 const heroPerson={
     fullName: 'Hasanuj Zaman',
@@ -26,12 +26,29 @@ const friendlyPerson={
     salary: 25000
 }
 
-const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
-heroChargeBill(5000);
-heroChargeBill(1000);
+// bind method
+// const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+// heroChargeBill(5000);
+// heroChargeBill(1000);
+// console.log(heroPerson.salary);
+
+// const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+// friendlyChargeBill(2000);
+// friendlyChargeBill(200);
+// console.log(friendlyPerson.salary);
+
+//call method 
+normalPerson.chargeBill.call(heroPerson, 2000, 200, 20);
 console.log(heroPerson.salary);
 
-const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
-friendlyChargeBill(2000);
-friendlyChargeBill(200);
+normalPerson.chargeBill.call(friendlyPerson, 5000, 500, 50);
+console.log(friendlyPerson.salary);
+
+//apply method
+normalPerson.chargeBill.apply(heroPerson, [2000, 200, 20]);
+normalPerson.chargeBill.apply(heroPerson, [1000, 500,  100]);
+console.log(heroPerson.salary);
+
+normalPerson.chargeBill.apply(friendlyPerson, [3000, 300, 30]);
+normalPerson.chargeBill.apply(friendlyPerson, [1000, 600, 70]);
 console.log(friendlyPerson.salary);
