a149739ddfd3a512b6570a0e762339bfdd56ba23
Hasib787 <mr.hasib45@gmail.com>
2021-02-05 21:14:37 +0600

    added bind object

--- call-apply.js
+++ call-apply.js
@@ -10,6 +10,28 @@ const normalPerson ={
         return this.salary;
     }
 }
-normalPerson.chargeBill(100);
-normalPerson.chargeBill(2000);
-console.log(normalPerson.salary);
+// normalPerson.chargeBill(100);
+// normalPerson.chargeBill(2000);
+// console.log(normalPerson.salary);
+
+const heroPerson={
+    fullName: 'Hasanuj Zaman',
+    phone: 6145478556,
+    salary: 20000
+}
+
+const friendlyPerson={
+    fullName: 'Iftekhar Hossain Noman',
+    phone: 1454715645,
+    salary: 25000
+}
+
+const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+heroChargeBill(5000);
+heroChargeBill(1000);
+console.log(heroPerson.salary);
+
+const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+friendlyChargeBill(2000);
+friendlyChargeBill(200);
+console.log(friendlyPerson.salary);
