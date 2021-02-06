5b3a803f09be55557e22df40fbddd0c4b5f860b2
Hasib787 <mr.hasib45@gmail.com>
2021-02-07 02:59:15 +0600

    added a setTimeout function and setInterval function

---
+++ timeout.js
@@ -0,0 +1,10 @@
+console.log(2222);
+console.log(3333);
+setTimeout(function(){
+    console.log('see you again');
+}, 4000);
+
+setInterval(() => {
+    console.log("do something");
+}, 3000);
+console.log(5555);
