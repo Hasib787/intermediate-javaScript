1bca3c256bd9aeaee55ef51cc47c9d65fa7de68d
Md. Masud Rana <mr.hasib45@gmail.com>
2023-02-01 11:44:06 +0600

    convert any value to boolean

---
+++ anyValueToBoolean.js
@@ -0,0 +1,10 @@
+//convert any value to boolean
+
+//Using !! in front of any value
+console.log(!!"Hasib"); //true
+console.log(!!1); //true
+console.log(!!0);  //false
+console.log(!!undefined);  //false
+
+// We can also use Boolean() to achieve the same
+console.log(Boolean("Hasib")); //true
